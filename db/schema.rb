73922729ec35284d780e743e28e44f49c5e66d02
Gatina Foy <foygatina@yahoo.com>
2016-04-25 15:58:01 -0400

    added ingredients, servings to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425185807) do
+ActiveRecord::Schema.define(version: 20160425195627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20160425185807) do
     t.datetime "updated_at",   null: false
     t.integer  "user_id",      null: false
     t.text     "instructions"
+    t.string   "ingredients"
+    t.string   "servings"
   end
 
   add_index "recipes", ["user_id"], name: "index_recipes_on_user_id", using: :btree
