204a9330db5ad258b20fa4c2a6f6f38fae05e804
Gatina Foy <foygatina@yahoo.com>
2016-04-28 19:27:13 -0400

    made chages to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
       thumb: '100x100>',
       square: '200x200#',
       medium: '300x300>'
-    }
+    } , default_url: ""
     # Validate the attached image is image/jpg, image/png, etc
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
