e34f1669a07df6e59e57df87a3ede7e161282e8a
Gatina Foy <foygatina@yahoo.com>
2016-04-29 09:53:09 -0400

    Assorted updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,10 +81,13 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_region => ENV['AWS_REGION'],
-    :bucket => ENV['S3_BUCKET_NAME'],
     :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
       :acess_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_acess_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
+    # Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+    # Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+    # Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-east-1.amazonaws.com'
   }
 end
