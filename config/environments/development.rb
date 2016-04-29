99836099288e30cf38645a2ad5d3a768066661be
Gatina Foy <foygatina@yahoo.com>
2016-04-29 09:01:53 -0400

    modified changes to heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,13 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+    config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_region => ENV['AWS_REGION'],
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :s3_credentials => {
+      :acess_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_acess_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_region => ENV['AWS_REGION'],
+    :bucket => ENV['S3_BUCKET_NAME'],
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
       :acess_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_acess_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
